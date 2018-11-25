7affe025fcd9c1b625e47f5c2c7a0c4e8cb6a5b9
Jakub Dorda <jak_dor@wp.pl>
2018-11-25 08:15:35 +0100

    add notification

--- app/src/main/java/com/asi/visahackyeah/App.kt
+++ app/src/main/java/com/asi/visahackyeah/App.kt
@@ -2,6 +2,10 @@ package com.asi.visahackyeah
 
 import android.app.Activity
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.asi.visahackyeah.di.AppInjector
 import com.asi.visahackyeah.utils.AppLogger
 import com.squareup.leakcanary.LeakCanary
@@ -26,8 +30,26 @@ class App : Application(), HasActivityInjector {
             return
         }
 
+        createNotificationChannel()
+
         LeakCanary.install(this)
         AppInjector.init(this)
         AppLogger.init(this)
+    }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.chanel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("123", name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
--- app/src/main/java/com/asi/visahackyeah/ui/MainActivity.kt
+++ app/src/main/java/com/asi/visahackyeah/ui/MainActivity.kt
@@ -2,15 +2,33 @@ package com.asi.visahackyeah.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import com.asi.visahackyeah.R
 import com.asi.visahackyeah.ui.payment.PaymentFragment
+import com.pusher.client.Pusher
+import com.pusher.client.PusherOptions
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 import timber.log.Timber
 import javax.inject.Inject
+import android.content.Context.NOTIFICATION_SERVICE
+import androidx.core.content.ContextCompat.getSystemService
+import android.app.NotificationManager
+import android.graphics.BitmapFactory
+import android.R.attr.data
+import android.app.Notification.DEFAULT_LIGHTS
+import android.app.Notification.DEFAULT_VIBRATE
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+
+
 
 class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
+
+    private var notificationId = 1;
 
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
@@ -22,6 +40,7 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        subscribe()
 
         lunchPaymentFragment()
     }
@@ -31,5 +50,39 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
             .add(R.id.mainFragmentLayout, PaymentFragment.getInstance(), PaymentFragment.CLASS_TAG)
             .commit()
         Timber.i("Lunched PaymentFragment")
+    }
+
+    fun subscribe(){
+        val options = PusherOptions()
+        options.setCluster("eu")
+        val pusher = Pusher("35db3a17fbbf7b8a74b6", options)
+
+        val channel = pusher.subscribe("my-channel")
+
+        channel.bind("my-event") { _, _, data ->
+
+            val contentIntent: PendingIntent
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("notifDisp", 1)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            contentIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+            val mBuilder = NotificationCompat.Builder(this,"default")
+                .setDefaults(DEFAULT_VIBRATE or DEFAULT_LIGHTS)
+                .setSmallIcon(R.drawable.menu_about)
+                .setContentTitle(title)
+                .setContentIntent(contentIntent)
+                .setContentText("Próba autoryzacji płatności")
+                .setAutoCancel(true)
+
+            val mNotificationManager = getSystemService(Co ntext.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationId++
+            mNotificationManager.notify(notificationId, mBuilder.build())
+
+            Timber.i("Received push notification: %s", data.toString())
+        }
+
+        pusher.connect()
     }
 }
